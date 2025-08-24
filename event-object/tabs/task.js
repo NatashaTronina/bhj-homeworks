2df4965db101a0437bb0aa265e8a3f16ca066545
Natasha Tronina <tronina.2004@yandex.ru>
2025-08-25 00:34:41 +0500

    Все работает

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const tabs = document.querySelectorAll('.tab');
-    const tabsContent = document.querySelectorAll('.tab__content');
+    const tabContainers = document.querySelectorAll('.tabs');
 
-    tabs.forEach((tab, index) => {
-        tab.addEventListener("click", () => {
-            tabs.forEach((tab) => tab.classList.remove('tab_active'));
-            tabsContent.forEach((content) => content.classList.remove('tab__content_active'));
-            tab.classList.add('tab_active');
-            tabsContent[index].classList.add('tab__content_active');
+    tabContainers.forEach(tab => {
+        const tabs = tab.querySelectorAll('.tab');
+        const tabsContent = tab.querySelectorAll('.tab__content');
+
+        tabs.forEach((tab, index) => {
+            tab.addEventListener("click", () => {
+                tabs.forEach((tab) => tab.classList.remove('tab_active'));
+                tabsContent.forEach((content) => content.classList.remove('tab__content_active'));
+                tab.classList.add('tab_active');
+                tabsContent[index].classList.add('tab__content_active');
+            });
         });
     });
 });
